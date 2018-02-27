e8b6b1fc6342a8a7765af25b8155b5bedc206853
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2018-02-27 18:14:51 +0200

    Jquery function fixed

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ jQuery(document).ready(function($){
 		this.bindEvents();
 	}
 
-	
+
 
 	radialSlider.prototype.updateIndexes = function(direction) {
 		if(  direction == 'next' ) {
@@ -56,7 +56,54 @@ jQuery(document).ready(function($){
 			//mask slide image to reveal navigation round element
 			this.slides.eq(this.visibleIndex).find('image').attr('style', 'mask: url(#'+this.leftMask.attr('id')+')');
 
+			function bezier(x1, y1, x2, y2, epsilon){
+				//https://github.com/arian/cubic-bezier
+				var curveX = function(t){
+					var v = 1 - t;
+					return 3 * v * v * t * x1 + 3 * v * t * t * x2 + t * t * t;
+				};
 
+				var curveY = function(t){
+					var v = 1 - t;
+					return 3 * v * v * t * y1 + 3 * v * t * t * y2 + t * t * t;
+				};
+
+				var derivativeCurveX = function(t){
+					var v = 1 - t;
+					return 3 * (2 * (t - 1) * t + v * v) * x1 + 3 * (- t * t * t + 2 * v * t) * x2;
+				};
+
+				return function(t){
+
+					var x = t, t0, t1, t2, x2, d2, i;
+
+					// First try a few iterations of Newton's method -- normally very fast.
+					for (t2 = x, i = 0; i < 8; i++){
+						x2 = curveX(t2) - x;
+						if (Math.abs(x2) < epsilon) return curveY(t2);
+						d2 = derivativeCurveX(t2);
+						if (Math.abs(d2) < 1e-6) break;
+						t2 = t2 - x2 / d2;
+					}
+
+					t0 = 0, t1 = 1, t2 = x;
+
+					if (t2 < t0) return curveY(t0);
+					if (t2 > t1) return curveY(t1);
+
+					// Fallback to the bisection method for reliability.
+					while (t0 < t1){
+						x2 = curveX(t2);
+						if (Math.abs(x2 - x) < epsilon) return curveY(t2);
+						if (x > x2) t0 = t2;
+						else t1 = t2;
+						t2 = (t1 - t0) * .5 + t0;
+					}
+
+					// Failure
+					return curveY(t2);
+
+				};
 
 		};
 	};
